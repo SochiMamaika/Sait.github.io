3a8ac79499aa7e7b86638a435ca923d732a880a2
Yaroslav Muravitskiy <yarik77.97@mail.ru>
2023-11-03 19:29:11 +0300

    update for mobile

--- app.js
+++ app.js
@@ -9,3 +9,9 @@ function simpleMath(a,b){
 }
 let sum = simpleMath(33, 52);
 console.log(sum);
+if (window.devicePixelRatio !== 1) { // Костыль для определения иных устройств, с коэффициентом отличным от 1		
+    var dpt = window.devicePixelRatio;
+    var widthM = window.screen.width * dpt;
+    var widthH = window.screen.height * dpt;
+    document.write('<meta name="viewport" content="width=' + widthM+ ', height=' + widthH + '">');
+  }
