f9b45db9ee1f9495f5ecc9b00b4d6af249ff18e5
Yarik <yarik77.97@mail.ru>
2025-06-02 19:54:35 +0300

    add_new_photo

--- app.js
+++ app.js
@@ -1,10 +1,13 @@
-
+// Функция переключения темы (тёмная ↔ светлая)
 function toggleTheme() {
     document.body.classList.toggle('light');
 }
-
   // Открытие модального окна при клике на фото
+  
+  // Получаем элемент модального окна по ID
   const modal = document.getElementById("modal");
+
+  // Получаем изображение внутри модального окна
   const modalImg = document.getElementById("modal-img");
   document.querySelectorAll(".gallery img").forEach(img => {
     img.addEventListener("click", () => {
@@ -15,9 +18,4 @@ function toggleTheme() {
 
   function closeModal() {
     modal.style.display = "none";
-  }
-
-  // Переключатель темы
-  function toggleTheme() {
-    document.body.classList.toggle('light');
   }
